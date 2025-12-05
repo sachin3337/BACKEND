b6820188a21d8e6b1312c246fc7ef09e4acb5d56
sachin ghusinge <sachinsubhashghusinge@gmail.com>
2025-12-05 17:07:36 +0530

    use of figlet

--- 03npm/script.js
+++ 03npm/script.js
@@ -8,3 +8,14 @@ figlet("Sachin Ghusinge", function (err, data) {
   }
   console.log(data);
 });
+
+figlet("Mayur Falke", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
+
+console.log("mayur Falke");

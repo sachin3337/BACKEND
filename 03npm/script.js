42b4260ac03adaa9dfbfa0c2ffa816eb9e90da91
sachin ghusinge <sachinsubhashghusinge@gmail.com>
2025-12-05 17:11:35 +0530

    figlet

--- 03npm/script.js
+++ 03npm/script.js
@@ -9,7 +9,17 @@ figlet("Sachin Ghusinge", function (err, data) {
   console.log(data);
 });
 
-figlet("Mayur Falke", function (err, data) {
+figlet("Subhash Ghusinge", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
+
+
+figlet("Jaydip Ghusinge", function (err, data) {
   if (err) {
     console.log("Something went wrong...");
     console.dir(err);
